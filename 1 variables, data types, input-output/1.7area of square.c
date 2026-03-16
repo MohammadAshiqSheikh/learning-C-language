19a3cf59774aaafd69a7c24e7d804a10b6f9767a
MohammadAshiqSheikh <ashiqsheikh484@gmail.com>
2026-03-16 16:15:44 +0530

    added the new program

---
+++ 1 variables, data types, input-output/1.7area of square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int side;
+  printf("please enter side of square ");
+  scanf("%d",&side);
+  printf("the area of square is :, %d ",side*side);
+
+
+
+  return 0;
+}
